0a4b5f6e4361d2772d528043c1cb8553dd04d87b
Elwardi <elwardifadeli@gmail.com>
2020-08-26 10:41:47 +0100

    Better names and identifiers for static checker runs

--- scripts/static_analyze.sh
+++ scripts/static_analyze.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 # Assuming CodeChecker is installed and sourced
-source ~/codechecker/venv/bin/activate
-export PATH=$PATH:~/codechecker/build/CodeChecker/bin/
 
-# fire up service
-CodeChecker server&
+branch="$1"
+commit="$2"
+if [ -z "$1" ]; then
+    branch="develop"
+    commit="0000000"
+fi
+
+source /opt/openfoam7/etc/bashrc
+
+set -x 
 
 # check the code
 CodeChecker check \
-    -b "./Allwclean; wmake libso src/rsr" \
+    -b "./Allwclean; ./Allwmake" \
     -i scripts/analyzer_skipfile \
     --checker-config \
     clang-tidy:cppcoreguidelines-special-member-functions.AllowMissingMoveFunctions=1\
@@ -17,12 +23,5 @@ CodeChecker check \
 
 # store check results
 CodeChecker store /tmp/static_results \
-    -n openrsr \
-    --url localhost:8001/OpenRSR
-
-# Generate html reports
-CodeChecker parse -e html /tmp/results -o ./reports_html
-
-rm -rf /tmp/static_results
-
-firefox http://localhost:8001
+    -n $commit --tag $branch \
+    --url http://openrsr-code-check.herokuapp.com:80/OpenRSR
