2749f3aa459abbbc2b46536cdfd380fee78e9f55
Elwardi <elwardifadeli@gmail.com>
2020-08-27 16:43:00 +0100

    Add base phase class and its tests. Works on #2

---
+++ src/rsr/phaseModels/phase/phase.C
@@ -0,0 +1,209 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     | Website:  https://openfoam.org
+    \\  /    A nd           | Copyright (C) 2011-2019 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "FVFModel.H"
+#include "dimensionedVector.H"
+#include "phase.H"
+#include "fixedValueFvsPatchField.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+template<class CompressibilityType, class ViscosityType>
+Foam::autoPtr<Foam::phase<CompressibilityType, ViscosityType>>
+Foam::phase<CompressibilityType, ViscosityType>::New
+(
+    const word& name,
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const mixtureType& mT
+)
+{
+    const word phaseType = dict.lookupOrDefault<word>
+    (
+        "phaseType",
+        "base"
+    );
+
+    typename dictionaryConstructorTable::iterator cstrIter =
+        dictionaryConstructorTablePtr_->find(phaseType);
+
+    if (cstrIter == dictionaryConstructorTablePtr_->end())
+    {
+        FatalErrorInFunction
+            << "Unknown phase type " << phaseType
+            << nl << nl
+            << "Valid phase types : " << nl
+            << dictionaryConstructorTablePtr_->sortedToc()
+            << exit(FatalError);
+    }
+
+    return autoPtr<phase<CompressibilityType, ViscosityType>>
+        (cstrIter()(name, mesh, dict, mT));
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+template<class CompressibilityType, class ViscosityType>
+Foam::phase<CompressibilityType, ViscosityType>::phase
+(
+    const word& name,
+    const fvMesh& mesh,
+    const dictionary& dict,
+    const mixtureType& mT
+)
+:
+    regIOobject
+    (
+        IOobject
+        (
+            name,
+            mesh.time().timeName(),
+            mesh,
+            IOobject::NO_READ,
+            IOobject::NO_WRITE
+        )
+    ),
+    name_(name),
+    phaseDict_(dict.subDict(name)),
+    mesh_(mesh),
+    U_
+    (
+        IOobject
+        (
+            name+".U",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedVector(name+".U0", dimVelocity, vector::zero)
+    ),
+    alphaPtr_
+    (
+        mT == mixtureType::multiPhase
+        ?
+        new volScalarField
+        (
+            IOobject
+            (
+                name+".alpha",
+                mesh.time().timeName(),
+                mesh,
+                IOobject::READ_IF_PRESENT,
+                IOobject::AUTO_WRITE
+            ),
+            mesh,
+            dimensionedScalar(name+".alpha", dimless, 1.0)
+        )
+        :
+        nullptr
+    ),
+    rho_
+    (
+        IOobject
+        (
+            name+".rho",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar(name+".rho0", dimless, 1.0) 
+    ),
+    phiPtr_
+    (
+        new surfaceScalarField
+        (
+           IOobject
+           (
+               name+".phi",
+               mesh.time().timeName(),
+               mesh,
+               IOobject::NO_READ,
+               IOobject::AUTO_WRITE
+           ),
+           linearInterpolate(U_) & mesh.Sf(),
+           fixedValueFvsPatchField<vector>::typeName
+        )
+    ),
+    mu_
+    (
+        IOobject
+        (
+            name+".rho",
+            mesh.time().timeName(),
+            mesh,
+            IOobject::READ_IF_PRESENT,
+            IOobject::AUTO_WRITE
+        ),
+        mesh,
+        dimensionedScalar(name+".rho0", dimless, 1.0) 
+    ),
+    BModel_
+    (
+        FVFModel<CompressibilityType>::New
+        (
+            name+".FVFModel", phaseDict_, mesh
+        )
+    )
+{
+}
+
+
+template<class CompressibilityType, class ViscosityType>
+Foam::phase<CompressibilityType, ViscosityType>::phase
+(
+    const phase& ph
+)
+:
+    regIOobject(ph),
+    name_(ph.name_),
+    phaseDict_(ph.phaseDict_),
+    mesh_(ph.mesh_),
+    U_(ph.U_),
+    alphaPtr_(ph.alphaPtr_),
+    rho_(ph.rho_),
+    phiPtr_(ph.phiPtr_),
+    mu_(ph.mu_),
+    BModel_
+    (
+        FVFModel<CompressibilityType>::New
+        (
+            name_+".FVFModel", phaseDict_, mesh_
+        )
+    )
+{
+}
+
+
+// * * * * * * * * * * * * * * * * Destructor  * * * * * * * * * * * * * * * //
+
+template<class CompressibilityType, class ViscosityType>
+Foam::phase<CompressibilityType, ViscosityType>::~phase()
+{}
+
+// ************************************************************************* //
---
+++ tests/rsr/phaseModels/phaseTest.C
@@ -0,0 +1,87 @@
+#include "catch.H"
+#include "fvCFD.H"
+#include "error.H"
+#include "samplePhase.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+
+using namespace Foam;
+using namespace phases;
+
+SCENARIO("Phase Object creation", "[Virtual]")
+{
+    GIVEN("Valid mesh and transportProperties")
+    {
+        #include "createTestTimeAndMesh.H"
+
+        word phaseName = "water";
+        dictionary transportProperties;
+        dictionary waterDict;
+        waterDict.add<word>("FVFModel", "incompressible");
+        transportProperties.add(phaseName, waterDict);
+
+        WHEN("Constructing phase in a singlePhase setup")
+        {
+            // Needs the presence of '0/water.U' dictionary
+            samplePhase water
+            (
+                phaseName,
+                mesh,
+                transportProperties,
+                samplePhase::mixtureType::singlePhase
+            );
+
+            THEN("Requesting a ref to alpha field errors out")
+            {
+                REQUIRE_THROWS(water.alpha());
+            }
+        }
+
+        WHEN("Constructing phase in a multiPhase setup")
+        {
+            samplePhase water
+            (
+                phaseName,
+                mesh,
+                transportProperties,
+                samplePhase::mixtureType::multiPhase
+            );
+
+            THEN("Requesting a ref to alpha field errors out")
+            {
+                REQUIRE_NOTHROW(water.alpha());
+            }
+        }
+
+        WHEN("Operating on the velocity of a stack-allocated phase object")
+        {
+            samplePhase water(phaseName, mesh, transportProperties);
+
+            surfaceScalarField& phi = water.phi();
+            volVectorField& U = water.U();
+
+            // Imitate a linear U profile
+            forAll(U, ci)
+            {
+                U[ci] = vector(mesh.nCells() - ci, 0, 0);
+            }
+            U.correctBoundaryConditions();
+
+            // Needs to set div(water.phi, water.U) in system/fvSchemes
+            water.correct();
+
+            THEN("Velocity must be constructed properly from phi "
+                    "using Weller's approximation")
+            {
+                // Particularly test if fvc::reconstruct has changed!
+                // fvc::reconstruct may be dissipative near some BCs
+                surfaceScalarField newPhi( linearInterpolate(U) & mesh.Sf() );
+                REQUIRE( phi.internalField()==newPhi.internalField() );
+            }
+        }
+
+    }
+}
+
+// ************************************************************************* //
