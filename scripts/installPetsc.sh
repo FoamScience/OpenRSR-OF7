c8d696bb866d7c3a1068c4ae846e9fa3d1ff620a
Elwardi <elwardifadeli@gmail.com>
2021-02-24 15:48:05 +0100

    Update compilation scripts

--- scripts/installPetsc.sh
+++ scripts/installPetsc.sh
@@ -4,9 +4,9 @@
 #==============================================================================
 
 # Install dependencies
-sudo apt update
-sudo apt-get install libatlas-base-dev libblas-dev \
-    liblapack-dev flex bison git make cmake gfortran
+#sudo apt update
+#sudo apt-get install libatlas-base-dev libblas-dev \
+#    liblapack-dev flex bison git make cmake gfortran
 
 set -vx
 
@@ -29,14 +29,17 @@ mpiCXX=$(which mpicxx)
 [ -z "$WM_PROJECT_DIR" ] && \
     { echo "\nERROR: Please source OpenFOAM's /etc/bashrc\n"; exit 1; }
 echo "\nDownloading Petsc v$PetscV \n"
-wget -c "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-$PetscV.tar.gz"
+#wget -c "http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-$PetscV.tar.gz"
 
 # Untar the file
 tar -xvzf petsc-$PetscV.tar.gz
 
 ## Configure petsc
 cd petsc-$PetscV 
-./configure --with-mpi-dir=$mpiDir --with-debugging=0 \
+./configure \
+    --with-debugging=0 --force \
+    --with-mpi-lib=\[$MPI_ARCH_PATH/lib/libmpi.so\] \
+    --with-mpi-include=\[$MPI_ARCH_PATH/include\] \
     --with-precision=double --with-shared-libraries=1 --with-scalar-type=real \
     --with-fc=$mpiFort --with-cc=$mpiCC --with-cxx=$mpiCXX \
     CXX_LINKER_FLAGS=-Wl,--no-as-needed \
@@ -46,10 +49,11 @@ cd petsc-$PetscV
     FFLAGS="-g -O2 -fstack-protector-strong" \
     CPPFLAGS="-Wdate-time -D_FORTIFY_SOURCE=2" \
     LDFLAGS="-Wl,-Bsymbolic-functions -Wl,-z,relro" MAKEFLAGS=w
+#   --with-mpi-dir=$mpiDir --with-debugging=0 --force \
 #    --download-hypre --download-parmetis --download-metis \
 #    --download-ptscotch --download-mumps --download-scalapack \
 
-echo "\nCompiling Petsc ..."
+echo "\n Compiling Petsc ..."
 
 parch=$(tail -200 configure.log | grep 'PETSC_ARCH:' | awk '{print $2}')
 pdir=$(tail -200 configure.log | grep 'PETSC_DIR:' | awk '{print $2}')
@@ -58,7 +62,8 @@ pdir=$(tail -200 configure.log | grep 'PETSC_DIR:' | awk '{print $2}')
 make PETSC_DIR=$pdir PETSC_ARCH=$parch all
 
 ## Source Petsc env. vars.
-echo "export PETSC_ARCH=$parch" >> $HOME/.bashrc
-echo "export PETSC_DIR=$pdir" >> $HOME/.bashrc
-echo "export LD_LIBRARY_PATH=\$PETSC_DIR/\$PETSC_ARCH/lib:\$LD_LIBRARY_PATH" >> $HOME/.bashrc
+mkdir -p etc
+echo "export PETSC_ARCH=$parch" > etc/bashrc
+echo "export PETSC_DIR=$pdir" >> etc/bashrc
+echo "export LD_LIBRARY_PATH=\$PETSC_DIR/\$PETSC_ARCH/lib:\$LD_LIBRARY_PATH" >> etc/bashrc
 echo "\nOK."
